9f6b10bab5aa180837cefb8059a3de40d960a0aa
stevanakva <stevanakvadrat@gmail.com>
2023-08-06 10:57:48 +0200

    izmene eslint

--- mongo.js
+++ mongo.js
@@ -17,31 +17,31 @@ mongoose.set('strictQuery',false)
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+	name: String,
+	number: String,
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 const person = new Person({
-  name: process.argv[3],
-  number: process.argv[4],
+	name: process.argv[3],
+	number: process.argv[4],
 })
 
 
 if(process.argv.length ===3){
-    Person.find({}).then(result => {
-        result.forEach(person => {
-            console.log(person);
-        });
-        mongoose.connection.close();
-    });
+	Person.find({}).then(result => {
+		result.forEach(person => {
+			console.log(person)
+		})
+		mongoose.connection.close()
+	})
 } else {
-    
 
-person.save().then(result => {
-    console.log(result)
-    console.log(`Person ${result.name} ${result.number} saved`);
-    mongoose.connection.close()
-  })
+
+	person.save().then(result => {
+		console.log(result)
+		console.log(`Person ${result.name} ${result.number} saved`)
+		mongoose.connection.close()
+	})
 }
